744729ef6546a48c1f664c53f37da13f8dc9847e
Suzanne <43080179+sl7211@users.noreply.github.com>
2024-03-23 18:27:04 -0400

    Initial commit

--- fractalu/app/routes/students.jake2.tsx
+++
@@ -1,5 +0,0 @@
-const Jake = () => {
-  return <div className="flex min-h-full flex-col justify-center text-5xl">Jake</div>;
-};
-
-export default Jake;
---
+++ fractalu/app/routes/students.suzanne.tsx
@@ -0,0 +1,5 @@
+const Jake = () => {
+  return <div className="flex min-h-full flex-col justify-center text-5xl">Suzanne</div>;
+};
+
+export default Jake;
