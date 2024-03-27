f493b697f33a6922076c97a3edab2bd4e32ad754
Sebastian Hodge <sebbyhodge@gmail.com>
2024-03-27 19:47:08 -0400

    cute little messsage

--- fractalu/app/routes/students.Sebastian.js
+++ fractalu/app/routes/students.Sebastian.js
@@ -1,20 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("~/styles/students.Sebastian.css");
 var react_1 = require("react");
-var MyComponent = function () {
-    return react_1.default.createElement(react_1.default.Fragment, null,
-        react_1.default.createElement("h3", null,
+require("./styles/students.Sebastian.css");
+var IMG_1912_jpg_1 = require("./IMG_1912.jpg");
+var SebastianPage = function () {
+    return (react_1.default.createElement("div", { className: "container" },
+        react_1.default.createElement("h1", { className: "header" }, "Sebastian's personal page"),
+        react_1.default.createElement("p", { className: "haiku" },
+            "A vibrant petal ",
             react_1.default.createElement("br", null),
-            "\"A vibrant petal,",
+            "floats down the rivering stream ",
             react_1.default.createElement("br", null),
-            "floats down the rivering stream",
-            react_1.default.createElement("br", null),
-            "of my flowing thoughts\""),
-        react_1.default.createElement("div", { className: "name" },
-            react_1.default.createElement("b", null, "Sebastian")),
-        react_1.default.createElement("a", { href: "https://www.instagram.com/sebastianhodge/?hl=en", target: "_blank" }, "Check out my art"),
-        react_1.default.createElement("br", null),
-        react_1.default.createElement("img", { src: "IMG_1912.jpg" }));
+            "of my flowing thoughts"),
+        react_1.default.createElement("img", { src: IMG_1912_jpg_1.default, alt: "Sebastian's Art", className: "artwork" }),
+        react_1.default.createElement("a", { className: "instagram-link", href: "https://www.instagram.com/sebastianhodge/?hl=en", target: "_blank" }, "Check out my art!")));
 };
-exports.default = MyComponent;
+exports.default = SebastianPage;
