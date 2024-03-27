b490342a06fb805eb3756584549013c40c179de6
Sebastian Hodge <sebbyhodge@gmail.com>
2024-03-27 15:00:54 -0400

    A haiku and a photo

--- fractalu/app/routes/students.Sebastian.tsx
+++ fractalu/app/routes/students.Sebastian.tsx
@@ -1,14 +1,21 @@
-import "~/styles/students.Sebastian.css"
 import React from 'react';
-const MyComponent = () => {
-        return <><h3><br/>"A vibrant petal,
-                    <br/>floats down the rivering stream
-                    <br/>of my flowing thoughts"</h3>
-                <div className="name">
-                    <b>Sebastian</b>
-                </div>
-                <a href="https://www.instagram.com/sebastianhodge/?hl=en" target="_blank">Check out my art</a>
-                <br/><img src="IMG_1912.jpg"/></>;
+import './styles/students.Sebastian.css'; 
+import sebastianArt from './IMG_1912.jpg'; 
+
+const SebastianPage = () => {
+    return (
+        <div className="container">
+            <h1 className="header">Sebastian's personal page</h1>
+            <p className="haiku">
+                A vibrant petal <br />
+                floats down the rivering stream <br />
+                of my flowing thoughts
+            </p>
+            <img src={sebastianArt} alt="Sebastian's Art" className="artwork" />
+            <a className="instagram-link" href="https://www.instagram.com/sebastianhodge/?hl=en" target="_blank">Check out my art!</a>
+        </div>
+    );
 };
 
-export default MyComponent;
+export default SebastianPage;
+
