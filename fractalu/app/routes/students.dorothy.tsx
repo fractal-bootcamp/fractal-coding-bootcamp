076dd3a0ed311e2d4368691fa55402788f7af9a1
Dorothy Ren <dorothyren@dorothys-air.mynetworksettings.com>
2024-03-27 19:46:57 -0400

    cute lil message

--- fractalu/app/routes/students.dorothy.tsx
+++ fractalu/app/routes/students.dorothy.tsx
@@ -1,5 +1,5 @@
-const Jake = () => {
-  return <div className="flex min-h-full flex-col justify-center">Jake</div>;
+const Dorothy = () => {
+  return <div className="flex min-h-full flex-col justify-center">Dorothy</div>;
 };
 
-export default Jake;
+export default Dorothy;
