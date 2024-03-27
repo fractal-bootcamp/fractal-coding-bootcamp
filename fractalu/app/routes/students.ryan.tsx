3227510f1a18e74ad00b87874908e81206e2e543
Ryan Zhuo <ryan@ryans-mbp-3.mynetworksettings.com>
2024-03-27 19:46:51 -0400

    hihi

--- fractalu/app/routes/students.ryan.tsx
+++ fractalu/app/routes/students.ryan.tsx
@@ -1,5 +1,5 @@
 const Ryan = () => {
-  return <div className="flex min-h-full flex-col justify-center">Ryan</div>;
+  return <div className="flex min-h-full flex-col justify-center text-5xl">Ryan</div>;
 };
 
 export default Ryan;
