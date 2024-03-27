f2450d2cbf0aab031f400b592800151f9b6a4965
alumni <alumni@sysgrwjwhl.lan>
2024-03-27 07:54:44 -0400

    replaced Jake with Anna

--- fractalu/app/routes/students.anna.tsx
+++ fractalu/app/routes/students.anna.tsx
@@ -1,5 +1,5 @@
-const Jake = () => {
-  return <div className="flex min-h-full flex-col justify-center">Jake</div>;
+const Anna = () => {
+  return <div className="flex min-h-full flex-col justify-center text-5xl">Anna</div>;
 };
 
-export default Jake;
+export default Anna;
