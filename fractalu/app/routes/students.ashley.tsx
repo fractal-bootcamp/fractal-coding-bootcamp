8d4d117abef5d3b377abfb0ffeb5df912f5722e8
Ashley Ding <ashleyding@ashleys-air-2.mynetworksettings.com>
2024-03-27 20:23:50 -0400

    first push

--- fractalu/app/routes/students.ashley.tsx
+++ fractalu/app/routes/students.ashley.tsx
@@ -1,4 +1,6 @@
 const Ashley = () => {
+  const textClass = isColored ? 'text-blue-500 font-serif' : 'text-black font-sans';
+
   return <div className="flex min-h-full flex-col justify-center text-5xl">Ashley</div>;
 };
 
