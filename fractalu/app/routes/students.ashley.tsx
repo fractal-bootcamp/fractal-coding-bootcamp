fb08039ac79f1a2213b913f62d27e80e1f3608cf
Ashley Ding <ashleyding@ashleys-air-2.mynetworksettings.com>
2024-03-27 20:37:57 -0400

    Push 2

--- fractalu/app/routes/students.ashley.tsx
+++ fractalu/app/routes/students.ashley.tsx
@@ -6,4 +6,23 @@ const Ashley = () => {
 
 const variable = 32;
 
+module.exports = {
+  //...
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+}
+
 export default Ashley;
--- fractalu/tailwind.config.ts
+++ fractalu/tailwind.config.ts
@@ -7,3 +7,8 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
+module.exports = {
+  //...
+  plugins: [require("daisyui")],
+}
